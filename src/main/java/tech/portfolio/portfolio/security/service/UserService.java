cdef35fd7219377c68572399411fb638a65ef69b
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:56:26 -0300

    Servicio User hecho

--- src/main/java/tech/portfolio/portfolio/security/service/UserService.java
+++ src/main/java/tech/portfolio/portfolio/security/service/UserService.java
@@ -1,13 +1,30 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.security.service;
 
-/**
- *
- * @author Francisco
- */
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.security.model.User;
+import tech.portfolio.portfolio.security.repo.UserRepository;
+
+@Service
+@Transactional
+@ComponentScan
 public class UserService {
-    
+
+    @Autowired
+    UserRepository userRepository;
+
+    public Optional<User> getByNameUser(String nameUser) {
+        return userRepository.findByNameUser(nameUser);
+    }
+
+    public boolean existsByNameUser(String nameUser) {
+        return userRepository.existsByNameUser(nameUser);
+    }
+
+    public void save(User user) {
+        userRepository.save(user);
+    }
 }
