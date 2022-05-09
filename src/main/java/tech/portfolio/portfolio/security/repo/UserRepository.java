2a03c1baa1077b1217b9654520afb0cc1596b4a5
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:52:08 -0300

    Repo User hecho

--- src/main/java/tech/portfolio/portfolio/security/repo/UserRepository.java
+++ src/main/java/tech/portfolio/portfolio/security/repo/UserRepository.java
@@ -1,13 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
 package tech.portfolio.portfolio.security.repo;
 
-/**
- *
- * @author Francisco
- */
-public interface UserRepository {
-    
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import tech.portfolio.portfolio.security.model.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByNameUser(String nameUser);
+
+    boolean existsByNameUser(String NameUser);
 }
