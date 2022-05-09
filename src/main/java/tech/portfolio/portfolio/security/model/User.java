81e3e1dbff1d9d88f10f8a6a1ac35b5c3f9b677b
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:41:58 -0300

    model Usuario hecho

--- src/main/java/tech/portfolio/portfolio/security/model/User.java
+++ src/main/java/tech/portfolio/portfolio/security/model/User.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.security.model;
 
-/**
- *
- * @author Francisco
- */
+import com.sun.istack.NotNull;
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.*;
+
 public class User {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(unique = true)
+    private String nameUser;
+
+    @NotNull
+    private String password;
+
+    @NotNull
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "userRole", joinColumns = @JoinColumn(name = "userId"),
+            inverseJoinColumns = @JoinColumn(name = "rolId"))
+    private Set<Role> roles = new HashSet<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String nameUser, String password) {
+        this.id = id;
+        this.name = name;
+        this.nameUser = nameUser;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNameUser() {
+        return nameUser;
+    }
+
+    public void setNameUser(String nameUser) {
+        this.nameUser = nameUser;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
