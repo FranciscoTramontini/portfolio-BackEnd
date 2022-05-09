d333face791edf00648f32c757763680c4d22751
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:00:05 -0300

    Servicio de experiencia hecho

--- src/main/java/tech/portfolio/portfolio/service/ExperienceService.java
+++ src/main/java/tech/portfolio/portfolio/service/ExperienceService.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.service;
 
-/**
- *
- * @author Francisco
- */
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.exception.NotFoundException;
+import tech.portfolio.portfolio.model.Experience;
+import tech.portfolio.portfolio.repo.ExperienceRepo;
+
+@Transactional
+@Service
 public class ExperienceService {
-    
+
+    private final ExperienceRepo experienceRepo;
+
+    @Autowired
+    public ExperienceService(ExperienceRepo experienceRepo) {
+        this.experienceRepo = experienceRepo;
+    }
+
+    public Experience addExperience(Experience experience) {
+        return experienceRepo.save(experience);
+    }
+
+    public List<Experience> findAllExperiences() {
+        return experienceRepo.findAll();
+    }
+
+    public Experience updateExperience(Experience experience) {
+        return experienceRepo.save(experience);
+    }
+
+    public Experience findExperienceById(Long id) {
+        return experienceRepo.findExperienceById(id).
+                orElseThrow(() -> new NotFoundException("Experience by id " + id + " was not found"));
+    }
+
+    public void deleteExperience(Long id) {
+        experienceRepo.deleteExperienceById(id);
+    }
 }
