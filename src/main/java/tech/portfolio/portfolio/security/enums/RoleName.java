41930f17d98fa1b7a3cdde9a0c7514535eeda660
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:22:04 -0300

    Creacion de enum RoleName vacio

---
+++ src/main/java/tech/portfolio/portfolio/security/enums/RoleName.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package tech.portfolio.portfolio.security.enums;
+
+/**
+ *
+ * @author Francisco
+ */
+public enum RoleName {
+    
+}
