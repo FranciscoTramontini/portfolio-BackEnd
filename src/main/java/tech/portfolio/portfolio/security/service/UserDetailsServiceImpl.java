6895866f2d6ccbfbf218735f003237ec83304b33
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 17:02:14 -0300

    Servicio UserDetailsServiceImpl hecho

--- src/main/java/tech/portfolio/portfolio/security/service/UserDetailsServiceImpl.java
+++ src/main/java/tech/portfolio/portfolio/security/service/UserDetailsServiceImpl.java
@@ -1,13 +1,23 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.security.service;
 
-/**
- *
- * @author Francisco
- */
-public class UserDetailsServiceImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.security.model.Admin;
+import tech.portfolio.portfolio.security.model.User;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nameUser) throws UsernameNotFoundException {
+        User user = userService.getByNameUser(nameUser).get();
+
+        return Admin.build(user);
+    }
 }
