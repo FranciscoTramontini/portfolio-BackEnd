152cf67ca46d5317e67447975badb6f0650849e9
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:57:49 -0300

    Servicio Role hecho

--- src/main/java/tech/portfolio/portfolio/security/service/RoleService.java
+++ src/main/java/tech/portfolio/portfolio/security/service/RoleService.java
@@ -1,13 +1,25 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.security.service;
 
-/**
- *
- * @author Francisco
- */
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.security.enums.RoleName;
+import tech.portfolio.portfolio.security.model.Role;
+import tech.portfolio.portfolio.security.repo.RoleRepository;
+
+@Service
+@Transactional
 public class RoleService {
-    
+
+    @Autowired
+    RoleRepository roleRepository;
+
+    public Optional<Role> getByRoleName(RoleName roleName) {
+        return roleRepository.findByRoleName(roleName);
+    }
+
+    public void save(Role role) {
+        roleRepository.save(role);
+    }
 }
