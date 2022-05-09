b06d84cfff9494432baaa9a25bd938d01a2a06c6
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:38:34 -0300

    enum RoleName hecho

--- src/main/java/tech/portfolio/portfolio/security/enums/RoleName.java
+++ src/main/java/tech/portfolio/portfolio/security/enums/RoleName.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
- */
 package tech.portfolio.portfolio.security.enums;
 
-/**
- *
- * @author Francisco
- */
 public enum RoleName {
-    
+    ROLE_ADMIN, ROLE_USER
 }
