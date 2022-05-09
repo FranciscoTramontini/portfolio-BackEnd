2c08c286c278ce703f81371398ccf5ed94528d7f
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 16:44:31 -0300

    model Role hecho

--- src/main/java/tech/portfolio/portfolio/security/model/Role.java
+++ src/main/java/tech/portfolio/portfolio/security/model/Role.java
@@ -1,13 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.security.model;
 
-/**
- *
- * @author Francisco
- */
+import tech.portfolio.portfolio.security.enums.RoleName;
+import com.sun.istack.NotNull;
+import javax.persistence.*;
+
 public class Role {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private RoleName roleName;
+
+    public Role() {
+    }
+
+    public Role(RoleName roleName) {
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
 }
