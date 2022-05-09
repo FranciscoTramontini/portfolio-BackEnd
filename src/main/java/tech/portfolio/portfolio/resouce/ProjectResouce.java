76e8e75a1542e125bc3919f1d00c960a0e905ea5
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 10:51:09 -0300

    creacion de la clase ProjectResouce vacio

---
+++ src/main/java/tech/portfolio/portfolio/resouce/ProjectResouce.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tech.portfolio.portfolio.resouce;
+
+/**
+ *
+ * @author Francisco
+ */
+public class ProjectResouce {
+    
+}
