fd2d006e0eaf59c239954efb15616acba60a0ac5
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 12:09:51 -0300

    Servicio de proyecto hecho

--- src/main/java/tech/portfolio/portfolio/service/ProjectService.java
+++ src/main/java/tech/portfolio/portfolio/service/ProjectService.java
@@ -1,13 +1,42 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.service;
 
-/**
- *
- * @author Francisco
- */
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.exception.NotFoundException;
+import tech.portfolio.portfolio.model.Project;
+import tech.portfolio.portfolio.repo.ProjectRepo;
+
+@Transactional
+@Service
 public class ProjectService {
-    
+
+    private final ProjectRepo projectRepo;
+
+    @Autowired
+    public ProjectService(ProjectRepo projectRepo) {
+        this.projectRepo = projectRepo;
+    }
+
+    public Project addProject(Project project) {
+        return projectRepo.save(project);
+    }
+
+    public List<Project> findAllProjects() {
+        return projectRepo.findAll();
+    }
+
+    public Project updateProject(Project project) {
+        return projectRepo.save(project);
+    }
+
+    public Project findProjectById(Long id) {
+        return projectRepo.findProjectById(id).
+                orElseThrow(() -> new NotFoundException("Project by id " + id + " was not found"));
+    }
+
+    public void deleteProject(Long id) {
+        projectRepo.deleteProjectById(id);
+    }
 }
