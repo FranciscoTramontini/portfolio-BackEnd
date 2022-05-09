59926d00462a83cf64261ac6ac444a405697e320
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 11:10:55 -0300

    Servicio de persona hecho

--- src/main/java/tech/portfolio/portfolio/service/PersonService.java
+++ src/main/java/tech/portfolio/portfolio/service/PersonService.java
@@ -1,13 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package tech.portfolio.portfolio.service;
 
-/**
- *
- * @author Francisco
- */
+import java.util.List;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.portfolio.portfolio.exception.NotFoundException;
+import tech.portfolio.portfolio.model.Person;
+import tech.portfolio.portfolio.repo.PersonRepo;
+
+@Transactional
+@Service
 public class PersonService {
-    
+    private final PersonRepo personRepo;
+
+    @Autowired
+    public PersonService(PersonRepo personRepo) {
+        this.personRepo = personRepo;
+    }
+
+    public Person addPerson(Person person) {
+        return personRepo.save(person);
+    }
+
+    public List<Person> findAllPersons() {
+        return personRepo.findAll();
+    }
+
+    public Person updatePerson(Person person) {
+        return personRepo.save(person);
+    }
+
+    public Person findPersonById(Long id) {
+        return personRepo.findPersonById(id).
+                orElseThrow(() -> new NotFoundException("Person by id " + id + " was not found"));
+    }
+
+    public void deletePerson(Long id) {
+        personRepo.deletePersonById(id);
+    }
 }
