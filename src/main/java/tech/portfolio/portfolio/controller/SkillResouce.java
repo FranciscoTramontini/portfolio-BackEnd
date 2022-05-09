d21b6a29fd7c444d2acb6356a4234672225af4f1
FranciscoTramontini <frantramontini97@gmail.com>
2022-05-09 11:24:15 -0300

    Controlador de persona hecho

--- src/main/java/tech/portfolio/portfolio/controller/SkillResouce.java
+++ src/main/java/tech/portfolio/portfolio/controller/SkillResouce.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tech.portfolio.portfolio.resouce;
+package tech.portfolio.portfolio.controller;
 
 /**
  *
